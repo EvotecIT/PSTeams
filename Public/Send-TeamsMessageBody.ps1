5203a6d667193ef7dcf0f9c6074edb732b892fc5
PrzemyslawKlys <pklys@evotec.pl>
2018-10-04 19:31:33 +0200

    Small update

--- Public/Send-TeamsMessageBody.ps1
+++ Public/Send-TeamsMessageBody.ps1
@@ -1,10 +1,10 @@
 function Send-TeamsMessageBody {
     param (
-        [string] $uri,
-        [string] $body
+        [alias("TeamsID")][Parameter(Mandatory = $true)][string]$URI,
+        [string] $Body
     )
     try {
-        $Execute = Invoke-RestMethod -uri $uri -Method Post -body $body -ContentType 'application/json'
+        $Execute = Invoke-RestMethod -URI $URI -Method Post -Body $Body -ContentType 'application/json'
     } catch {
         $ErrorMessage = $_.Exception.Message -replace "`n", " " -replace "`r", " "
         Write-Warning "Send-TeamsMessageBody - Failed with error message: $ErrorMessage"
