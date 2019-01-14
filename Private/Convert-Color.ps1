b6539c1da2e80e9e8a96b19e25b1b6553a31a8a0
PrzemyslawKlys <pklys@evotec.pl>
2019-01-14 22:22:29 +0100

    Colors will be removed later on as stuff got moved to PSSharedGoods.

---
+++ Private/Convert-Color.ps1
@@ -0,0 +1,59 @@
+function Convert-Color {
+    <#
+    .Synopsis
+    This color converter gives you the hexadecimal values of your RGB colors and vice versa (RGB to HEX)
+    .Description
+    This color converter gives you the hexadecimal values of your RGB colors and vice versa (RGB to HEX). Use it to convert your colors and prepare your graphics and HTML web pages.
+    .Parameter RBG
+    Enter the Red Green Blue value comma separated. Red: 51 Green: 51 Blue: 204 for example needs to be entered as 51,51,204
+    .Parameter HEX
+    Enter the Hex value to be converted. Do not use the '#' symbol. (Ex: 3333CC converts to Red: 51 Green: 51 Blue: 204)
+    .Example
+    .\convert-color -hex FFFFFF
+    Converts hex value FFFFFF to RGB
+
+    .Example
+    .\convert-color -RGB 123,200,255
+    Converts Red = 123 Green = 200 Blue = 255 to Hex value
+
+    #>
+    param(
+        [Parameter(ParameterSetName = "RGB", Position = 0)]
+        [ValidateScript( {$_ -match '^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$'})]
+        $RGB,
+        [Parameter(ParameterSetName = "HEX", Position = 0)]
+        [ValidateScript( {$_ -match '[A-Fa-f0-9]{6}'})]
+        [string]
+        $HEX
+    )
+    switch ($PsCmdlet.ParameterSetName) {
+        "RGB" {
+            if ($RGB[2] -eq $null) {
+                Write-error "Value missing. Please enter all three values seperated by comma."
+            }
+            $red = [convert]::Tostring($RGB[0], 16)
+            $green = [convert]::Tostring($RGB[1], 16)
+            $blue = [convert]::Tostring($RGB[2], 16)
+            if ($red.Length -eq 1) {
+                $red = '0' + $red
+            }
+            if ($green.Length -eq 1) {
+                $green = '0' + $green
+            }
+            if ($blue.Length -eq 1) {
+                $blue = '0' + $blue
+            }
+            Write-Output $red$green$blue
+        }
+        "HEX" {
+            $red = $HEX.Remove(2, 4)
+            $Green = $HEX.Remove(4, 2)
+            $Green = $Green.remove(0, 2)
+            $Blue = $hex.Remove(0, 4)
+            $Red = [convert]::ToInt32($red, 16)
+            $Green = [convert]::ToInt32($green, 16)
+            $Blue = [convert]::ToInt32($blue, 16)
+            Write-Output $red, $Green, $blue
+        }
+    }
+}
--- Private/ConvertFrom-Color.ps1
+++ Private/ConvertFrom-Color.ps1
@@ -5,15 +5,18 @@ function ConvertFrom-Color {
         [RGBColors] $Color,
         [switch] $AsDecimal
     )
-
     $Value = $Script:RGBColors."$Color"
-    Write-Verbose "Convert-FromColor - Color Name: $Color Value: $Value"
+    $HexValue = Convert-Color -RGB $Value
+    Write-Verbose "Convert-FromColor - Color Name: $Color Value: $Value HexValue: $HexValue"
+    <#
+    [string] $HexVal = ''
     foreach ($arg in $Value) {
         $hexval = $hexval + [Convert]::ToString($arg, 16).ToUpper()
     }
+    #>
     if ($AsDecimal) {
-        return  [Convert]::ToInt64($hexval,16)
+        return  [Convert]::ToInt64($HexValue, 16)
     } else {
-        return "#$($hexval)" # .Substring(2)
+        return "#$($HexValue)"
     }
 }
