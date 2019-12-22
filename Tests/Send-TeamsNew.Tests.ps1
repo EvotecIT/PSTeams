46bbcedfdb68316e8843a7579cf5e9f4846c7624
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-22 20:49:13 +0100

    New test

---
+++ Tests/Send-TeamsNew.Tests.ps1
@@ -0,0 +1,22 @@
+﻿param (
+    $TeamsID = $Env:TEAMSPESTERID
+)
+#Requires -Modules Pester
+Import-Module $PSScriptRoot\..\PSTeams.psd1 -Force #-Verbose
+
+Describe 'Send-TeamsMessage - Should send messages properly with new syntax' {
+    It 'Given 3 facts, 1 section should not throw' {
+        Send-TeamsMessage -URI $TeamsID -MessageTitle 'PSTeams - Pester Test' -MessageText "This text will show up" -Color DodgerBlue {
+            New-TeamsSection {
+                New-TeamsActivityTitle -Title "**PSTeams**"
+                New-TeamsActivitySubtitle -Subtitle "@PSTeams - $CurrentDate"
+                New-TeamsActivityImage -Image Add
+                New-TeamsActivityText -Text "This message proves PSTeams Pester test passed properly."
+                New-TeamsFact -Name 'PS Version' -Value "**$($PSVersionTable.PSVersion)**"
+                New-TeamsFact -Name 'PS Edition' -Value "**$($PSVersionTable.PSEdition)**"
+                New-TeamsFact -Name 'OS' -Value "**$($PSVersionTable.OS)**"
+                New-TeamsButton -Name 'Visit English Evotec Website' -Link "https://evotec.xyz"
+            }
+        }
+    }
+}
