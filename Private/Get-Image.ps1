9c2e91cd5ff9bdd75c42e9d4e01ce7cb4317dc8c
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-22 10:11:29 +0100

    Not needed check

--- Private/Get-Image.ps1
+++ Private/Get-Image.ps1
@@ -6,18 +6,18 @@ function Get-Image {
         [string] $FileExtension
     )
     Write-Verbose "Get-Image - PathToImages $PathToImages FileName $FileName FileExtension $FileExtension"
-    if ($ImageType -ne [ImageType]::None) {
-        $ImagePath = [IO.Path]::Combine( $PathToImages, "$($FileName)$FileExtension")
-        Write-Verbose "Get-Image - ImagePath $ImagePath"
-        if (Test-Path $ImagePath) {
-            if ($PSEdition -eq 'Core') {
-                $Image = [convert]::ToBase64String((Get-Content $ImagePath -AsByteStream))
-            } else {
-                $Image = [convert]::ToBase64String((Get-Content $ImagePath -Encoding byte))
-            }
-            Write-Verbose "Get-Image - Image Type: $($Image.GetType())"
-            return "data:image/png;base64,$Image"
+    #if ($ImageType -ne [ImageType]::None) {
+    $ImagePath = [IO.Path]::Combine( $PathToImages, "$($FileName)$FileExtension")
+    Write-Verbose "Get-Image - ImagePath $ImagePath"
+    if (Test-Path $ImagePath) {
+        if ($PSEdition -eq 'Core') {
+            $Image = [convert]::ToBase64String((Get-Content $ImagePath -AsByteStream))
+        } else {
+            $Image = [convert]::ToBase64String((Get-Content $ImagePath -Encoding byte))
         }
+        Write-Verbose "Get-Image - Image Type: $($Image.GetType())"
+        return "data:image/png;base64,$Image"
     }
+    # }
     return ''
 }
