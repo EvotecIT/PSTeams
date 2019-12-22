038b2e63382c3c27e985ff7581e7dbb8f866eef5
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-22 12:26:58 +0100

    New examples

---
+++ Examples/Example-DSLStyle.ps1
@@ -0,0 +1,51 @@
+﻿Import-Module $PSScriptRoot\..\PSTeams.psd1 -Force #-Verbose
+
+# This is fake TeamsID - you need to use yours
+$TeamsID = 'https://outlook.office.ad05-32e40'
+
+Send-TeamsMessage -Verbose -Color DimGray {
+    New-TeamsSection -Title 'This is 1st section within 1 message' {
+        New-TeamsFact -Name 'Bold' -Value '**Special GPO**'
+        New-TeamsFact -Name 'Italic and Bold' -Value '***Other values***'
+        New-TeamsFact -Name 'Italic' -Value '*2010-10-10*'
+    } #-ActivityTitle "**Przemyslaw Klys**" #-ActivitySubtitle "@przemyslawklys - 9/12/2016 at 5:33pm" -ActivityImageLink "https://github.com/EvotecIT/PSTeams/blob/master/Images/10.png?raw=true" -ActivityText "Climate change explained in comic book form by xkcd xkcd.com/1732" `
+    New-TeamsSection -Title 'This is 2nd section within 1 message' {
+        New-TeamsFact -Name 'Bold' -Value '**Special GPO**'
+        New-TeamsFact -Name 'Italic and Bold' -Value '***Other values***'
+        New-TeamsFact -Name 'Italic' -Value '*2010-10-10*'
+        New-TeamsFact -Name 'Link example' -Value "[Microsoft](https://www.microsoft.com)"
+        New-TeamsFact -Name 'Other link example' -Value "[Evotec](https://evotec.xyz) and some **bold** text"
+        New-TeamsList -Name 'Testing List' {
+            New-TeamsListItem -Text 'Test 1' -Level 0 -Numbered
+            New-TeamsListItem -Text 'Test 2' -Level 1 -Numbered
+            New-TeamsListItem -Text 'Test 3' -Level 1 -Numbered
+            New-TeamsListItem -Text 'Test 4' -Level 2 -Numbered
+            New-TeamsListItem -Text 'Test 5' -Level 2 -Numbered
+            New-TeamsListItem -Text 'Test 6' -Level 2 -Numbered
+            New-TeamsListItem -Text 'Test 7' -Level 0 -Numbered
+            New-TeamsListItem -Text 'Test 8' -Level 0 -Numbered
+        }
+        New-TeamsList -Name 'Testing List' {
+            New-TeamsListItem -Text 'Test 1' -Level 0
+            New-TeamsListItem -Text 'Test 2' -Level 1
+            New-TeamsListItem -Text 'Test 3' -Level 1
+            New-TeamsListItem -Text 'Test 4' -Level 2
+            New-TeamsListItem -Text 'Test 5' -Level 2
+            New-TeamsListItem -Text 'Test 6' -Level 2
+            New-TeamsListItem -Text 'Test 7' -Level 0
+            New-TeamsListItem -Text 'Test 8' -Level 0
+        }
+        New-TeamsList -Name 'Testing List' {
+            New-TeamsListItem -Text 'First ordered list item' -Level 0
+            New-TeamsListItem -Text 'Another item' -Level 1  -Numbered
+            New-TeamsListItem -Text 'First ordered list item' -Level 2
+            New-TeamsListItem -Text 'First ordered list item' -Level 3
+            New-TeamsListItem -Text 'Unordered sub-list' -Level 0
+            New-TeamsListItem -Text "Actual numbers don't matter, just that it's a number" -Level 0 -Numbered
+            New-TeamsListItem -Text 'Ordered sub-list' -Level 1 -Numbered
+            New-TeamsListItem -Text 'Another entry' -Level 1 -Numbered
+            New-TeamsListItem -Text 'Another entry' -Level 0 -Numbered
+            New-TeamsListItem -Text 'Very very long line that I want to show, hellow helow. Very very long line that I want to show, hellow helow. ' -Level 0 -Numbered
+        }
+    } -Uri $TeamsID -MessageSummary 'Test1' #-MessageText 'Test2' -MessageTitle 'Test1' #
+}
---
+++ Examples/Example-TweetLike.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module $PSScriptRoot\..\PSTeams.psd1 -Force #-Verbose
+
+$TeamsID = 'https://outlook.office.com/webhook/a5c7c'
+
+Send-TeamsMessage -Verbose {
+    New-TeamsSection -ActivityTitle "**Elon Musk**" -ActivitySubtitle "@elonmusk - 9/12/2016 at 5:33pm" -ActivityImageLink "https://pbs.twimg.com/profile_images/782474226020200448/zDo-gAo0.jpg" -ActivityText "Climate change explained in comic book form by xkcd xkcd.com/1732"
+    New-TeamsSection -ActivityTitle "**Mark Knopfler**" -ActivitySubtitle "@MarkKnopfler - 9/12/2016 at 1:12pm" -ActivityImageLink "https://pbs.twimg.com/profile_images/1042367841117384704/YvrqQiBK_400x400.jpg" -ActivityText "Mark Knopfler features on B.B King's all-star album of Blues greats, released on this day in 2005..."
+    New-TeamsSection -ActivityTitle "**Elon Musk**" -ActivitySubtitle "@elonmusk - 9/12/2016 at 5:33pm" -ActivityImageLink "https://pbs.twimg.com/profile_images/782474226020200448/zDo-gAo0.jpg" -ActivityText "Climate change explained in comic book form by xkcd xkcd.com/1732"
+} -Uri $TeamsID -Color DarkSeaGreen -MessageSummary 'Tweet'
