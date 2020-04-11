bff5f042085e40cd1ece8093f0c9596e419eb0cb
Ram Iyer <ramakrishnan.rkology@gmail.com>
2020-04-11 09:43:14 +0530

    Detect hashtable or PSObject from input Any other common datatype will throw an error

--- Public/ConvertTo-TeamsFact.ps1
+++ Public/ConvertTo-TeamsFact.ps1
@@ -1,46 +1,48 @@
 function ConvertTo-TeamsFact {
     <#
     .SYNOPSIS
-    Convert a PSCustomObject to Teams facts.
+    Convert a PSCustomObject or a Hashtable to Teams facts.
     
     .DESCRIPTION
-    Teams facts are a name-value pair. This module helps convert a PSObject to Teams facts (only string values, one level deep).
+    Teams facts are name-value pairs. This module helps convert a PSObject or a Hashtable to Teams facts (only one level deep).
     
     .PARAMETER InputObject
-    The PSObject that is output by another cmdlet.
+    The Hashtable or PSObject that is output by another cmdlet.
     
     .EXAMPLE
     Get-ChildItem | Select-Object -First 1 | ConvertTo-TeamsFact
+
+    .EXAMPLE
+    @{ Product = 'Microsoft Teams'; Developer = 'Microsoft Corporation'; ReleaseYear = '2018' } | ConvertTo-TeamsFact
     
     .NOTES
-    Ram Iyer (ram.iyer@merckgroup.com)
+    Ram Iyer (https://ramiyer.me)
     #>
     
-    [CmdletBinding(DefaultParameterSetName = 'InputObject')]
+    [CmdletBinding()]
     param (
         # The input object
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0, ParameterSetName = 'InputObject')]
-        [PSObject]
-        $InputObject,
-
-        # A dictionary input
-        [Parameter(Mandatory = $true, ParameterSetName = 'InputHash')]
-        [hashtable]
-        $InputHash
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        $InputObject
     )
 
     begin { }
     process {
-        if ($InputObject) {
-            $Facts = foreach ($Property in $InputObject.PsObject.Properties) {
-                New-TeamsFact -Name $Property.Name -Value $Property.Value
+        if ($InputObject -is [hashtable]) {
+            $Facts = foreach ($Key in $InputObject.Keys) {
+                New-TeamsFact -Name $Key -Value $InputObject.$Key
             }
         }
-        elseif ($InputDictionary) {
-            $Facts = foreach ($Key in $InputDictionary.Keys) {
-                New-TeamsFact -Name $Key -Value $InputDictionary.$Key
+        elseif (($InputObject -is [int]) -or ($InputObject -is [long]) -or ($InputObject -is [string]) -or ($InputObject -is [char]) -or ($InputObject -is [bool]) -or ($InputObject -is [byte]) -or ($InputObject -is [double]) -or ($InputObject -is [decimal]) -or ($InputObject -is [single]) -or ($InputObject -is [array]) -or ($InputObject -is [xml])) {
+            Write-Error -Message 'The input is neither a PSObject nor a Hashtable. Operation aborted.' -Category InvalidData
+            break
+        }
+        elseif ($InputObject -is [psobject]) {
+            $Facts = foreach ($Property in $InputObject.PsObject.Properties) {
+                New-TeamsFact -Name $Property.Name -Value $Property.Value
             }
         }
         $Facts
     }
 }
+Get-Help ConvertTo-TeamsFact
