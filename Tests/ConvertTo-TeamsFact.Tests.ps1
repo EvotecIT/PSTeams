80b555d4f98784544a9e516a48eefe770851ddbe
Ram Iyer <ramakrishnan.rkology@gmail.com>
2020-04-12 11:43:32 +0530

    Execute all convert tests under the same context A matter of simplifying the output; cosmetic

--- Tests/ConvertTo-TeamsFact.Tests.ps1
+++ Tests/ConvertTo-TeamsFact.Tests.ps1
@@ -5,7 +5,7 @@ param (
 Import-Module $PSScriptRoot\..\PSTeams.psd1 -Force #-Verbose
 
 Describe "Conversion of different objects to Teams fact" {
-    Context "PSObject" {
+    Context "Object types: PSObject, unordered dictionary, ordered dictionary and string" {
         It "Convert PSObject to Teams facts" {
             $Output = New-Object -TypeName PSObject -Property ([ordered]@{
                     Application = 'Microsoft Teams'
@@ -16,8 +16,6 @@ Describe "Conversion of different objects to Teams fact" {
             $Output | Should -BeOfType 'System.Collections.Specialized.OrderedDictionary'
             $Output[0] | Should MatchHashTable @{ name = 'Application'; value = 'Microsoft Teams'; type = 'fact' } 
         }
-    }
-    Context "Unordered dictionary" {
         It "Convert unordered dictionary to Teams facts" {
             $Output = @{
                 Application = 'Microsoft Teams'
@@ -28,8 +26,6 @@ Describe "Conversion of different objects to Teams fact" {
             $Output | Should -BeOfType 'System.Collections.Specialized.OrderedDictionary'
             $Output[0].type | Should -Be 'fact'
         }
-    }
-    Context "Ordered dictionary" {
         It "Convert ordered dictionary to Teams facts" {
             $Output = [ordered]@{
                 Application = 'Microsoft Teams'
@@ -40,8 +36,6 @@ Describe "Conversion of different objects to Teams fact" {
             $Output | Should -BeOfType 'System.Collections.Specialized.OrderedDictionary'
             $Output[0] | Should MatchHashTable @{ name = 'Application'; value = 'Microsoft Teams'; type = 'fact' } 
         }
-    }
-    Context "String" {
         It "Throw an error when a plain string is passed in" {
             { 'My Plain String' | ConvertTo-TeamsFact } | Should Throw
         }
