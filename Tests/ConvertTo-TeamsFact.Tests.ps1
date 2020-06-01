ecd447be9df017e163f2da5ba4ede7ba1cbbe4df
Ram Iyer <ramakrishnan.rkology@gmail.com>
2020-06-01 12:23:30 +0530

    Fix tests; no more array input for TeamsFact

--- Tests/ConvertTo-TeamsFact.Tests.ps1
+++ Tests/ConvertTo-TeamsFact.Tests.ps1
@@ -14,7 +14,7 @@ Describe "Conversion of different objects to Teams fact" {
             ) | ConvertTo-TeamsFact
             $Output | Should -HaveCount 2
             $Output | Should -BeOfType 'System.Collections.Specialized.OrderedDictionary'
-            $Output[0].type | Should -Be 'fact'
+            $Output.type | Should -Be 'fact'
         }
         It "Convert unordered dictionary to Teams facts" {
             $Output = @{
@@ -24,7 +24,7 @@ Describe "Conversion of different objects to Teams fact" {
             
             $Output | Should -HaveCount 2
             $Output | Should -BeOfType 'System.Collections.Specialized.OrderedDictionary'
-            $Output[0].type | Should -Be 'fact'
+            $Output.type | Should -Be 'fact'
         }
         It "Convert ordered dictionary to Teams facts" {
             $Output = [ordered]@{
@@ -34,7 +34,7 @@ Describe "Conversion of different objects to Teams fact" {
             
             $Output | Should -HaveCount 2
             $Output | Should -BeOfType 'System.Collections.Specialized.OrderedDictionary'
-            $Output[0].type | Should -Be 'fact'
+            $Output.type | Should -Be 'fact'
         }
         It "Throw an error when a plain string is passed in" {
             { 'My Plain String' | ConvertTo-TeamsFact } | Should Throw
