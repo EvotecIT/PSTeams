c76d5ba983cce30ec7106b28e204f964591ae47e
Ram Iyer <ramakrishnan.rkology@gmail.com>
2020-06-01 12:10:37 +0530

    Convert array to Teams sections; better legibility

---
+++ Public/ConvertTo-TeamsSection.ps1
@@ -0,0 +1,43 @@
+function ConvertTo-TeamsSection {
+    <#
+    .SYNOPSIS
+    Convert an array of PSCustomObject or a Hashtable to separate Teams sections.
+    
+    .DESCRIPTION
+    Teams sections are chunks of information that appear within a Teams message. This function helps convert an array of PSObject or an array of Hashtables to Teams sections (only one level deep).
+    
+    .PARAMETER InputObject
+    The Hashtable or PSObject that is output by another cmdlet.
+    
+    .EXAMPLE
+    Get-ChildItem -Directory | ConvertTo-TeamsSection -SectionTitleProperty Name
+    
+    .NOTES
+    Ram Iyer (https://ramiyer.me)
+    #>
+    param (
+        # The input object
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        $InputObject,
+
+        # The property to use for title
+        [Parameter(Mandatory = $false, Position = 1)]
+        [string]
+        $SectionTitleProperty
+    )
+
+    process {
+        $TotalCount = $InputObject.Count
+        $CurrentCount = 1
+
+        foreach ($Item in $InputObject) {
+            $SectionParams = @{
+                ActivityDetails = $Item | ConvertTo-TeamsFact
+            }
+            if ($SectionTitleProperty) {
+                $SectionParams.ActivityTitle = "$(($SectionTitleProperty -creplace '([A-Z])', ' $1').Trim()) $($Item.$SectionTitleProperty)"
+            }
+            New-TeamsSection @SectionParams
+        }
+    }
+}
