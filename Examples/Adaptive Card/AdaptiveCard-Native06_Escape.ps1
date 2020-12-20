7ac1c263be015614eebdc071152211b966c1ba03
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-20 10:45:52 +0100

    Escape markdown example

---
+++ Examples/Adaptive Card/AdaptiveCard-Native06_Escape.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PSTeams.psd1 -Force
+
+New-AdaptiveCard -Uri $Env:TEAMSPESTERID -VerticalContentAlignment center {
+    New-AdaptiveColumnSet {
+        New-AdaptiveColumn {
+            New-AdaptiveTextBlock -Size 'Medium' -Text 'Test Card _Title_ 1' -Color Warning # this will trigger markdown
+            New-AdaptiveTextBlock -Size 'Medium' -Text "Test Card \_Title_ 1" -Color Warning # This works
+            New-AdaptiveTextBlock -Size 'Medium' -Text "Test Card \_Title\_ 1" -Color Warning # This works
+            $Text = "Test Card _Title_ 1" -replace '_','\_'
+            New-AdaptiveTextBlock -Size 'Medium' -Text $Text -Color Warning # This works
+        }
+    }
+} -Verbose
