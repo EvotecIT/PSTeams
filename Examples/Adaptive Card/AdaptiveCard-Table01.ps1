855988ac61b819a65ad5e16994c16dd5605d66e4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-19 23:22:37 +0200

    Add example showing how to use Adaptive Table

--- Examples/Adaptive Card/AdaptiveCard-Table01.ps1
+++ Examples/Adaptive Card/AdaptiveCard-Table01.ps1
@@ -1,26 +1,42 @@
 ﻿Import-Module .\PSTeams.psd1 -Force
 
-New-AdaptiveCard {
-    #New-TeamsText -Size 'Medium' -Weight Bolder -Text 'Now that we have defined the main rules and features of the format, we need to produce a schema and publish it to GitHub. The schema will be the starting point of our reference documentation.' -Separator -Wrap
-    New-AdaptiveColumnSet {
-        New-AdaptiveColumn {
-            New-AdaptiveFactSet {
-                New-AdaptiveFact -Title 'Board:1' -Value 'Adaptive Cards1'
-                New-AdaptiveFact -Title 'Board:2' -Value 'Adaptive Cards2'
-                New-AdaptiveFact -Title 'Board:4' -Value 'Adaptive Cards3'
-                New-AdaptiveFact -Title 'Board:5' -Value 'Adaptive Cards4'
-            }
-        } -Width stretch
-        New-AdaptiveColumn {
-            New-AdaptiveFactSet {
-                New-AdaptiveFact -Title 'Board:1' -Value 'Adaptive Cards1'
-                New-AdaptiveFact -Title 'Board:2' -Value 'Adaptive Cards2'
-                New-AdaptiveFact -Title 'Board:4' -Value 'Adaptive Cards3'
-                New-AdaptiveFact -Title 'Board:5' -Value 'Adaptive Cards4'
-            }
-        } -Width stretch
+$Objects = @(
+    [PSCustomObject] @{
+        Test  = 123
+        Test2 = "Tes1t"
     }
-    New-AdaptiveRichTextBlock -HorizontalAlignment left
-    New-AdaptiveTextBlock -Text 'Dear <at>przemyslawklys</at>' -Subtle -Spacing None
-    #New-AdaptiveTextBlock -Text '<at>Przemysław</at>'
-} -Uri $Env:TEAMSPESTERID
+    [PSCustomObject] @{
+        Test  = 456
+        Test2 = "Test2"
+    }
+    [PSCustomObject] @{
+        Test  = 789
+        Test2 = "Test3"
+    }
+)
+$ObjectsHashes = @(
+    [ordered] @{
+        Test  = 123
+        Test2 = "Tes1t"
+    }
+    [ordered] @{
+        Test  = 456
+        Test2 = "Test2"
+    }
+    [ordered] @{
+        Test  = 789
+        Test2 = "Test3"
+    }
+)
+
+$Card = New-AdaptiveCard {
+    New-AdaptiveTextBlock -Size 'Medium' -Weight Bolder -Text 'Table usage with PSCustomObject' -Separator -Wrap
+
+    New-AdaptiveTable -DataTable $Objects
+
+    New-AdaptiveTextBlock -Size 'Medium' -Weight Bolder -Text 'Table usage with OrderedDictionary' -Separator -Wrap
+
+    New-AdaptiveTable -DataTable $ObjectsHashes
+} -Uri $Env:TEAMSPESTERID -FullWidth -ReturnJson
+
+$Card | ConvertFrom-Json | ConvertTo-Json -Depth 20
