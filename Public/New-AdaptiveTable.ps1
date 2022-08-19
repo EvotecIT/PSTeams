9f5276f40a3992a7c7db7d6dccd21eb386789adf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-19 23:22:58 +0200

    Add adaptive tables

---
+++ Public/New-AdaptiveTable.ps1
@@ -0,0 +1,66 @@
+﻿function New-AdaptiveTable {
+    <#
+    .SYNOPSIS
+    Simplifies process of creating an adaptive table by working with PowerShell objects
+
+    .DESCRIPTION
+    Simplifies process of creating an adaptive table by working with PowerShell objects
+
+    .PARAMETER DataTable
+    Provide a data table to be converted to an adaptive table
+
+    .PARAMETER HeaderColor
+    Provide a color to be used for the header row of the table. By default, the header row is set to 'Accent'
+
+    .PARAMETER HeaderWeight
+    Provide a weight to be used for the header row of the table. By default, the header row is set to 'Bolder'
+
+    .EXAMPLE
+    $Card = New-AdaptiveCard {
+        New-AdaptiveTextBlock -Size 'Medium' -Weight Bolder -Text 'Table usage with PSCustomObject' -Separator -Wrap
+
+        New-AdaptiveTable -DataTable $Objects
+
+        New-AdaptiveTextBlock -Size 'Medium' -Weight Bolder -Text 'Table usage with OrderedDictionary' -Separator -Wrap
+
+        New-AdaptiveTable -DataTable $ObjectsHashes
+    } -Uri $Env:TEAMSPESTERID
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        [Array] $DataTable,
+        [ValidateSet("Accent", 'Default', 'Dark', 'Light', 'Good', 'Warning', 'Attention')][string] $HeaderColor = 'Accent',
+        [alias('FontWeight')][ValidateSet("Lighter", 'Default', "Bolder")][string] $HeaderWeight = 'Bolder'
+    )
+
+    if ($DataTable[0] -is [System.Collections.IDictionary]) {
+        New-AdaptiveColumnSet {
+            for ($i = 0; $i -lt $DataTable[0].Keys.Count; $i++) {
+                New-AdaptiveColumn {
+                    $HeaderText = @($DataTable[0].Keys)[$i]
+                    New-AdaptiveTextBlock -Text $HeaderText -Weight $HeaderWeight -Color $HeaderColor
+                    for ($j = 0; $j -lt $DataTable.Count; $j++) {
+                        $Test = @($DataTable[$j].Values)[$i]
+                        New-AdaptiveTextBlock -Text $Test -Separator
+                    }
+                } -Width Stretch
+            }
+        }
+    } else {
+        New-AdaptiveColumnSet {
+            for ($i = 0; $i -lt $DataTable[0].PSObject.Properties.Name.Count; $i++) {
+                New-AdaptiveColumn {
+                    $HeaderText = $DataTable[0].PSObject.Properties.Name[$i]
+                    New-AdaptiveTextBlock -Text $HeaderText -Weight $HeaderWeight -Color $HeaderColor
+                    for ($j = 0; $j -lt $DataTable.Count; $j++) {
+                        $Test = $DataTable[$j].PSObject.Properties.Value[$i]
+                        New-AdaptiveTextBlock -Text $Test -Separator
+                    }
+                } -Width Stretch
+            }
+        }
+    }
+}
