e8f3628ea11a5b6d9e5ef97a946edf466a249635
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-20 09:07:42 +0200

    Improve/fix New-AdaptiveTable

--- Public/New-AdaptiveTable.ps1
+++ Public/New-AdaptiveTable.ps1
@@ -15,6 +15,9 @@
     .PARAMETER HeaderWeight
     Provide a weight to be used for the header row of the table. By default, the header row is set to 'Bolder'
 
+    .PARAMETER DictionaryAsCustomObject
+    Forces display of Dictionary the same way as a custom object. By default, the Dictionary is displayed the way you see with Format-Table
+
     .EXAMPLE
     $Card = New-AdaptiveCard {
         New-AdaptiveTextBlock -Size 'Medium' -Weight Bolder -Text 'Table usage with PSCustomObject' -Separator -Wrap
@@ -33,18 +36,41 @@
     param(
         [Array] $DataTable,
         [ValidateSet("Accent", 'Default', 'Dark', 'Light', 'Good', 'Warning', 'Attention')][string] $HeaderColor = 'Accent',
-        [alias('FontWeight')][ValidateSet("Lighter", 'Default', "Bolder")][string] $HeaderWeight = 'Bolder'
+        [alias('FontWeight')][ValidateSet("Lighter", 'Default', "Bolder")][string] $HeaderWeight = 'Bolder',
+        [alias('HashTableAsCustomObject')][switch] $DictionaryAsCustomObject
     )
 
     if ($DataTable[0] -is [System.Collections.IDictionary]) {
-        New-AdaptiveColumnSet {
-            for ($i = 0; $i -lt $DataTable[0].Keys.Count; $i++) {
+        if ($DictionaryAsCustomObject) {
+            New-AdaptiveColumnSet {
+                for ($i = 0; $i -lt $DataTable[0].Keys.Count; $i++) {
+                    New-AdaptiveColumn {
+                        $HeaderText = @($DataTable[0].Keys)[$i]
+                        New-AdaptiveTextBlock -Text $HeaderText -Weight $HeaderWeight -Color $HeaderColor
+                        for ($j = 0; $j -lt $DataTable.Count; $j++) {
+                            $Test = @($DataTable[$j].Values)[$i]
+                            New-AdaptiveTextBlock -Text $Test -Separator
+                        }
+                    } -Width Stretch
+                }
+            }
+        } else {
+            New-AdaptiveColumnSet {
                 New-AdaptiveColumn {
-                    $HeaderText = @($DataTable[0].Keys)[$i]
-                    New-AdaptiveTextBlock -Text $HeaderText -Weight $HeaderWeight -Color $HeaderColor
-                    for ($j = 0; $j -lt $DataTable.Count; $j++) {
-                        $Test = @($DataTable[$j].Values)[$i]
-                        New-AdaptiveTextBlock -Text $Test -Separator
+                    New-AdaptiveTextBlock -Text 'Name' -Weight $HeaderWeight -Color $HeaderColor
+                    foreach ($Data in $DataTable) {
+                        foreach ($Key in $Data.Keys) {
+                            New-AdaptiveTextBlock -Text $Key -Separator
+                        }
+                    }
+                } -Width Stretch
+
+                New-AdaptiveColumn {
+                    New-AdaptiveTextBlock -Text 'Value' -Weight $HeaderWeight -Color $HeaderColor
+                    foreach ($Data in $DataTable) {
+                        foreach ($Key in $Data.Keys) {
+                            New-AdaptiveTextBlock -Text $Data[$Key] -Separator
+                        }
                     }
                 } -Width Stretch
             }
